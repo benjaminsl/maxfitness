3aac7bb27086dde14bb2dcdb4e53fbb64a257e80
Benjamin Leach <benjaminstephenleach@gmail.com>
2018-09-28 20:53:15 -0500

    added javascript file to highligh selected option

--- readinesscheckin.js
+++ readinesscheckin.js
@@ -1,3 +1,9 @@
+var sore_score = 0;
+var stressed_score = 0;
+var energy_score = 0;
+var mood_score = 0;
+var sleep_score = 0;
+
 function highlight_this_button(clicked_id)
 {
 	document.getElementById(clicked_id).style.backgroundColor = "yellow";
